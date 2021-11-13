d751d61f51406bef2469cff7b8a9aae2da669931
tuyenf <tuyenf.1999@gmail.com>
2021-11-13 16:04:09 +0700

    Change

--- dist/js/menu.js
+++ dist/js/menu.js
@@ -1,8 +1,10 @@
 // Menu
 const menuBtn = document.querySelector(".menu-toggle");
 const menu = document.querySelector(".menu-list");
+const headerSearch = document.querySelector(".header-search");
 menuBtn.addEventListener("click", () => {
   menu.classList.toggle("isShow");
+  headerSearch.classList.toggle("isShow");
 });
 document.addEventListener("click", handleMenuClick);
 function handleMenuClick(e) {
