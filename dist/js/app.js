6525da964fad6acc5a1de44ab7d0a96824f2a322
tuyenf <tuyenf.1999@gmail.com>
2021-11-13 15:35:48 +0700

    mobile-responsive

--- dist/js/app.js
+++ dist/js/app.js
@@ -46,3 +46,24 @@ const heroInfo = document.querySelector(".hero-info");
     heroInfo.style.height = "auto";
   }, 2000);
 })();
+
+// Scroll to top
+const scrollTopBtn = document.querySelector(".scroll-to-top-button");
+window.onscroll = function () {
+  handleScroll();
+};
+function handleScroll() {
+  if (
+    document.body.scrollTop > 600 ||
+    document.documentElement.scrollTop > 600
+  ) {
+    scrollTopBtn.style.display = "block";
+  } else {
+    scrollTopBtn.style.display = "none";
+  }
+}
+
+scrollTopBtn.addEventListener("click", function () {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+});
