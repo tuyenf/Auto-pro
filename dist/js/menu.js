bbb795746e9f8f648b1c3469574d5c516d0ad58f
tuyenf <tuyenf.1999@gmail.com>
2021-11-17 12:02:40 +0700

    add overlay, modify button

--- dist/js/menu.js
+++ dist/js/menu.js
@@ -2,12 +2,15 @@
 const menuBtn = document.querySelector(".menu-toggle");
 const menu = document.querySelector(".menu-list");
 const headerSearch = document.querySelector(".header-search");
+const overlay = document.querySelector(".overlay");
 menuBtn.addEventListener("click", () => {
   menu.classList.toggle("isShow");
+  overlay.classList.toggle("isShow")
 });
-document.addEventListener("click", handleMenuClick);
+overlay.addEventListener("click", handleMenuClick);
 function handleMenuClick(e) {
   if (!menu.contains(e.target) && !e.target.matches(".menu-toggle")) {
     menu.classList.remove("isShow");
+    e.target.classList.remove("isShow");
   }
 }
